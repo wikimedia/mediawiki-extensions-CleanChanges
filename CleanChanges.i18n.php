38848a38678fab2cb503d01117ca985c60ec3078
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-03 22:12:16 +0000

    Localisation updates for extension messages from Betawiki (2008-02-03 22:50 CET)

--- CleanChanges.i18n.php
+++ CleanChanges.i18n.php
@@ -12,6 +12,46 @@ $messages['en'] = array(
 	'cleanchanges-desc'             => 'More compact display of [[Special:RecentChanges|recent changes list]]',
 );
 
+/** Bulgarian (Български)
+ * @author DCLXVI
+ */
+$messages['bg'] = array(
+	'cleanchanges-desc' => 'По-компактно показване на [[Special:RecentChanges|списъка с последните промени]]',
+);
+
 $messages['fi'] = array(
 	'cleanchanges-changerightslink' => 'muokkaa ryhmiä',
 );
+
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'cleanchanges-changerightslink' => 'change les groupes',
+	'cleanchanges-desc'             => "Plus d'affichage compact de la [[Special:RecentChanges|liste des modifications récentes]]",
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'cleanchanges-changerightslink' => 'skupiny změnić',
+	'cleanchanges-desc'             => 'Kompaktniše zwobraznjenje [[Special:RecentChanges|lisćiny aktualnych změnow]]',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'cleanchanges-changerightslink' => 'groepen wijzigen',
+	'cleanchanges-desc'             => 'Meer compacte weergave van de [[Special:RecentChanges|lijst met recente wijzigingen]]',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'cleanchanges-changerightslink' => 'zmeniť skupiny',
+	'cleanchanges-desc'             => 'Kompaktnejšie zobrazenie [[Special:RecentChanges|zoznamu posledných úprav]]',
+);
+
