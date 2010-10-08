2952a986fa9b0312d4867ae0e66c3ddab9421449
Raimond Spekking <raymond@users.mediawiki.org>
2010-10-08 21:40:52 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-10-08 21:23:00 UTC)

--- CleanChanges.i18n.php
+++ CleanChanges.i18n.php
@@ -1007,6 +1007,7 @@ $messages['szl'] = array(
  * @author TRYPPN
  */
 $messages['ta'] = array(
+	'cleanchanges-desc' => '[[Special:RecentChanges|அண்மைய மாற்றங்களின் பட்டியல்]] மிகச் சுருக்கமாக காட்டப்பட்டுள்ளது',
 	'cleanchanges-language' => 'மொழி:',
 	'cleanchanges-language-na' => 'அனைத்தும்',
 	'cleanchanges-users' => 'பயனர்கள் (Sep: |):',
