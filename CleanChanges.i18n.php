7646b8cd710840e472bbca3abc352a9253202e98
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-01-22 20:08:34 +0000

    Localisation updates for extensions from translatewiki.net.

--- CleanChanges.i18n.php
+++ CleanChanges.i18n.php
@@ -105,7 +105,7 @@ $messages['ast'] = array(
 	'cleanchanges-language' => 'Llingua:',
 );
 
-/** Bashkir (Башҡорт)
+/** Bashkir (Башҡортса)
  * @author Assele
  */
 $messages['ba'] = array(
@@ -117,7 +117,7 @@ $messages['ba'] = array(
 	'cleanchanges-showuserlinks' => 'Ҡатнашыусы һылтанмаларын күрһәтергә',
 );
 
-/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
  * @author EugeneZelenko
  * @author Jim-by
  */
@@ -635,7 +635,7 @@ $messages['ksh'] = array(
 	'cleanchanges-showuserlinks' => 'Links op Metmaacher zeije',
 );
 
-/** Cornish (Kernewek)
+/** Cornish (Kernowek)
  * @author Kw-Moon
  */
 $messages['kw'] = array(
@@ -986,7 +986,7 @@ $messages['sl'] = array(
 	'cleanchanges-showuserlinks' => 'Prikaži uporabniške povezave',
 );
 
-/** Serbian Cyrillic ekavian (Српски (ћирилица))
+/** Serbian Cyrillic ekavian (‪Српски (ћирилица)‬)
  * @author Sasa Stefanovic
  * @author Михајло Анђелковић
  */
@@ -999,7 +999,7 @@ $messages['sr-ec'] = array(
 	'cleanchanges-showuserlinks' => 'Покажи корисничке линкове',
 );
 
-/** Serbian Latin ekavian (Srpski (latinica))
+/** Serbian Latin ekavian (‪Srpski (latinica)‬)
  * @author Michaello
  */
 $messages['sr-el'] = array(
